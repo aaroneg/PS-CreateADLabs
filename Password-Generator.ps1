19c52b6877f0d39ae20d01606dc1c33a06dd3488
Aaron <aaroneg@users.noreply.github.com>
2018-03-27 14:17:59 -0500

    Create Password-Generator.ps1

---
+++ Password-Generator.ps1
@@ -0,0 +1,13 @@
+function New-BadPassword ([int]$PassLength=10) {
+    $ValidChars="A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",`
+                "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",`
+                "1","2","3","4","5","6","7","8","9","0","$","%","&","(",")","*","+","-","/",":",";","<","=",">","?","@",`
+                "[","\","]","^","_","{","}","~"
+    $i=0
+    $password='a1@'
+    while ($i -le $passlength) {
+        $password+=Get-Random $ValidChars
+        $i++
+    }
+    $password
+}
