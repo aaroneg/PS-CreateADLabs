4d47fee77425ee7f20fb61a5ae0a0ffe1cae6bd6
Aaron <aaroneg@users.noreply.github.com>
2018-05-09 15:36:20 -0500

    Update 00-New-DevelopmentEnvironment.ps1

--- 00-New-DevelopmentEnvironment.ps1
+++ 00-New-DevelopmentEnvironment.ps1
@@ -1,7 +1,10 @@
+# Change directory to the running user's documents folder
 Set-Location $env:userprofile\Documents\
+# Install several modules to enable git in powershell
 Install-Module posh-git -Force
 install-module chocolatey
 install-chocolateysoftware
 install-chocolateypackage git
 Import-Module posh-git
+# Make sure posh-git is loaded every time powershell is opened
 'import-module posh-git'|out-file $PROFILE -Append
