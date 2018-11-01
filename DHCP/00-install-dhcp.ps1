1d06303647b04acfc73172843579777fef988a1e
Aaron <aaroneg@users.noreply.github.com>
2018-11-01 13:44:59 -0500

    Update 00-install-dhcp.ps1

--- DHCP/00-install-dhcp.ps1
+++ DHCP/00-install-dhcp.ps1
@@ -1,6 +1,12 @@
 # Add the role to windows
 Install-windowsfeature dhcp
+# Create the DHCP security groups
+netsh dhcp add securitygroups
 # Get the hostname
 $hostname=hostname
 # Authorize the DHCP server in the domain
 add-dhcpserverindc -dnsname $hostname
+# Restart the dhcpserver service
+Restart-service dhcpserver
+Set-DhcpServerv4DnsSetting -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
+
