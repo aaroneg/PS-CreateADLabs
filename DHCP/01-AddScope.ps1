baffd19508edb0b1467437310f2ea53f4363c264
Aaron <aaroneg@users.noreply.github.com>
2018-11-01 14:25:13 -0500

    Update 01-AddScope.ps1

--- DHCP/01-AddScope.ps1
+++ DHCP/01-AddScope.ps1
@@ -9,4 +9,9 @@ $ScopeArguments=@{
   State="Active"
   Type="Dhcp"
 }
-Add-DHCPServerv4Scope @ScopeArguments -Verbose
+Add-DHCPServerv4Scope @ScopeArguments -Verbose 
+
+# Set the default gateway on the scope
+Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.0.1 -ScopeID 192.168.0.0
+# Set the dns server & Name on the scope
+Set-DhcpServerv4OptionValue -DnsDomain corp.contoso.com -DnsServer 192.168.0.2
