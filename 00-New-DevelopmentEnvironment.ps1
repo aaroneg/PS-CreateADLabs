3b499d3babc1957fc7fe6e744c922a31f646c764
Aaron <aaron@aaroneg.com>
2019-10-29 00:43:13 -0500

    Upgrade init script.

--- 00-New-DevelopmentEnvironment.ps1
+++ 00-New-DevelopmentEnvironment.ps1
@@ -1,10 +1,17 @@
 # Change directory to the running user's documents folder
 Set-Location $env:userprofile\Documents\
-# Install several modules to enable git in powershell
+# Make sure the powershell profile exists
+if (!(Test-Path $PROFILE)) {
+    ""|Out-File $PROFILE
+}
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module posh-git -Force
 install-module chocolatey
 install-chocolateysoftware
-install-chocolateypackage git
+install-chocolateypackage git -Force -Confirm:$false
+install-chocolateypackage vscode -Force -Confirm:$false
 Import-Module posh-git
 # Make sure posh-git is loaded every time powershell is opened
 'import-module posh-git'|out-file $PROFILE -Append
